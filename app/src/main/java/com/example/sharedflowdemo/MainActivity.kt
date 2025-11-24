ddfafb1799913087ecdaa4b214b7ffe77cbf43d9
Admin <vadimsuman55@gmail.com>
2025-11-24 23:48:57 +0700

    Сбор данных о потоке

--- app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
+++ app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
@@ -9,9 +9,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sharedflowdemo.ui.theme.SharedFlowDemoTheme
+import kotlinx.coroutines.flow.SharedFlow
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,18 +44,41 @@ fun ScreenSetup(
     modifier: Modifier = Modifier,
     viewModel: DemoViewModel = viewModel()
 ) {
-    MainScreen(modifier)
+    MainScreen(modifier, viewModel.sharedFlow)
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-}
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    sharedFlow: SharedFlow<Int>
+) {
+    val messages = remember { mutableStateListOf<Int>() }
+    val lifecycleOwner = LocalLifecycleOwner.current
 
+    LaunchedEffect(key1 = Unit) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            sharedFlow.collect {
+                messages.add(it)
+            }
+        }
+    }
+
+    LazyColumn(modifier = modifier) {
+        items(messages) { value ->
+            Text(
+                "Collected Value = $value",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(5.dp)
+            )
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SharedFlowDemoTheme {
-
+        val viewModel: DemoViewModel = viewModel()
+        MainScreen(sharedFlow = viewModel.sharedFlow)
     }
 }
